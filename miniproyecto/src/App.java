c3f2d23cf10f86b2a26b07709a292017698bad2d
gmorcillog <gmorcillog02@iesalbarregas.es>
2025-03-27 00:42:38 +0100

    Solucion gestor

--- miniproyecto/src/App.java
+++ miniproyecto/src/App.java
@@ -1,9 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        XML xml = new XML();
-        GestorDatos gestor = new GestorDatos();
-
-        xml.escribirFichero(gestor);
-        System.out.println(gestor.toString());
+        System.out.println("Hello, World!");
     }
 }
