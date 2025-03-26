76c39706d6b8f9c6c778c01bfc3254cb60c41296
gmorcillog <gmorcillog02@iesalbarregas.es>
2025-03-27 00:36:24 +0100

    Clase XML

--- miniproyecto/src/App.java
+++ miniproyecto/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        XML xml = new XML();
+        GestorDatos gestor = new GestorDatos();
+
+        xml.escribirFichero(gestor);
+        System.out.println(gestor.toString());
     }
 }
