8ac3e4636233d84eb1f87e5e078aac227032788b
gloriajean02 <gcuradog01@iesalbarregas.es>
2025-03-27 17:11:13 +0100

    ajustes develop

--- miniproyecto/src/Conversor.java
+++ miniproyecto/src/Conversor.java
@@ -3,6 +3,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map.Entry;
 
 public class Conversor {
@@ -21,9 +22,74 @@ public class Conversor {
     public Conversor(String rutaArchivo, GestorDatos gestor){
         this.rutaArchivo = rutaArchivo;
         this.gestor = gestor;
+      
     }
 
-    // Método que convierte los datos del GestorDatos a un archivo CSV
+    public Conversor(GestorDatos gestor, String elementoRaiz, String elementoItem) {
+        this.rutaArchivo = "miniproyecto/fichero/datos.xml";
+        this.gestor = gestor;
+        this.elementoRaiz = elementoRaiz;
+        this.elementoItem = elementoItem;
+    }
+
+    public Conversor(String rutaArchivo, GestorDatos gestor, String elementoRaiz, String elementoItem) {
+        this.rutaArchivo = rutaArchivo;
+        this.gestor = gestor;
+        this.elementoRaiz = elementoRaiz;
+        this.elementoItem = elementoItem;
+    }
+
+    public void convertirXML() {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(rutaArchivo))) {
+            bw.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+            bw.write("<" + elementoRaiz + ">\n");
+
+            for (HashMap<String, String> elemento : gestor.getGestor()) {
+                bw.write("  <" + elementoItem + ">\n");
+                for (String clave : elemento.keySet()) {
+                    bw.write("    <" + clave + ">" + elemento.get(clave) + "</" + clave + ">\n");
+                }
+                bw.write("  </" + elementoItem + ">\n");
+            }
+
+            bw.write("</" + elementoRaiz + ">\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void conversorJSON(){
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(rutaArchivo, true))) {
+            bw.write("[\n");
+
+            ArrayList<HashMap<String, String>> listadeHashmaps = gestor.getGestor();
+            for (int i = 0; i < listadeHashmaps.size(); i++) {
+                HashMap<String, String> elemento = listadeHashmaps.get(i);
+
+                bw.write("  {\n");
+                int contadorLineasHashmap = 0;
+
+                for (Entry<String, String> entry : elemento.entrySet()){
+                    bw.write("      \""+entry.getKey()+"\": "+"\""+entry.getValue()+"\"");
+                    if (contadorLineasHashmap < elemento.size()-1) {
+                        bw.write(",");
+                    }
+                    bw.write("\n");
+                    contadorLineasHashmap++;
+                }
+                bw.write("  }");
+                if (i < listadeHashmaps.size()-1) {
+                    bw.write(",");
+                }
+                bw.write("\n");
+            }
+            bw.write("]\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+
     public void conversorCSV() {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(rutaArchivo))) {
 
@@ -51,12 +117,16 @@ public class Conversor {
                             bw.write(","); // Agregar coma entre los valores
                         }
                 }
-                bw.write("\n");
+                bw.newLine();
+                }
+                System.out.println("Archivo CSV creado correctamente en: " + rutaArchivo);
+            } else {
+                System.out.println("No hay datos en el Gestor para convertir.");
             }
-            bw.write("]\n");
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 }
+
